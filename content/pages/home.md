dfdd3969d010ddd129c5d63d0b808b46a3fe36ef
Goran Mekić <meka@tilda.center>
2017-11-26 17:59:06 +0100

    Pelican based static site

---
+++ content/pages/home.md
@@ -0,0 +1,9 @@
+Title: Goran Mekić
+Date: 2015-06-06 22:00
+URL:
+save_as: index.html
+status: hidden
+
+- Co-founder of [Tilda Center](http://tilda.center) - Hackerspace in Novi Sad
+- Guitar player and singer of [Lust for Trust](https://www.facebook.com/lustfortrust)
+- Vice president of [LUGoNS](https://lugons.org)
---
+++ pelicanconf.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'ahrimana'
+SITENAME = u'ahrimana'
+SITESUBTITLE = u'Thoughts of a hacker and a musician'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Belgrade'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/atom.xml'
+CATEGORY_FEED_ATOM = 'feeds/categories/%s/atom.xml'
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+TAG_FEED_ATOM = 'feeds/tags/%s/atom.xml'
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (
+    ('github', 'https://github.com/mekanix'),
+    ('twitter', 'https://twitter.com/meka_floss'),
+    ('linkedin', 'https://www.linkedin.com/in/goran-mekić-b1030120/'),
+    ('envelope', 'mailto:meka@tilda.center'),
+    ('rss', FEED_ALL_ATOM),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = 'theme'
+DELETE_OUTPUT_DIRECTORY = True
+INDEX_SAVE_AS = 'blog/index.html'
+DELETE_OUTPUT_DIRECTORY = True
+DISQUS_SITENAME = "mekars"
+GITHUB_URL = "https://github.com/mekanix"
+TWITTER_URL = "https://twitter.com/meka_floss"
+ARTICLE_URL = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/'
+ARTICLE_SAVE_AS = 'blog/{date:%Y}/{date:%m}/{date:%d}/{slug}/index.html'
+MENUITEMS = (
+    ('blog', '/blog'),
+)
+STATIC_PATHS = [
+    'extra/favicon.ico',
+    'images',
+]
+EXTRA_PATH_METADATA = {
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+}
+PROFILE_IMAGE = "avatar.png"
+BIO = "I am Goran Mekić, and I'm FreeBSD user and enthusiast, musician and hacker"
